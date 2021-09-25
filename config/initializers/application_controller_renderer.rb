782fe45357a8691dd67fdd98108df373871f4c4f
utdmn2 <utdmn2@gmail.com>
2021-09-25 08:35:29 +0000

    [Add]index

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'aecdf6519b785c8a267df18ee1d1a72af95f430cc6e5a38992b7507a03a2787a77c7dff9c851333205ac394c8d6b32aa2d7d5f7ff8b2ae3e93df11d1a7b4f323'
